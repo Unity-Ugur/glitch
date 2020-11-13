ca7f07ffebf80095f28d0df2129b44fab38c1c40
Uğur Müslim <ugur@sistemkoin.com>
2020-11-13 21:11:59 +0300

    starting point

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minWaitingTime = 1f;
+    [SerializeField] float maxWaitingTime = 3f;
+    [SerializeField] Attacker attackerPrefab;
+    private bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitingTime, maxWaitingTime));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour
+{
+   [SerializeField] private int waitingTime = 3;
+   int currentSceneIndex;
+
+   public void Start()
+   {
+      currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+      if (currentSceneIndex == 0)
+      {
+         StartCoroutine(LoadNextScreenAfterWait());
+      }      
+   }
+   IEnumerator LoadNextScreenAfterWait()
+   {
+      yield return new WaitForSeconds(waitingTime);
+      LoadNextScene();
+   }
+
+   private void LoadNextScene()
+   {
+      SceneManager.LoadScene(currentSceneIndex + 1);
+   }
+
+   public void LoadLoadingScreen()
+   {
+      SceneManager.LoadScene("Splash Scene");
+   }
+}
+
